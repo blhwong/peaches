87c35abb89de00f10c19e8acaa7763dd541055de
Brandon Wong <b.lh.wong@gmail.com>
2017-05-08 23:35:47 -0700

    Create loading page section

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="profile-container">
+          <img className="profile" alt="Brandon Wong" src="profile.jpg"/>
+          <h1>Brandon Wong</h1>
+          <div className="sub-heading">I'm a software engineer, full-stack JavaScript developer, bird enthusiast, and a displaced Texan living in<br/>San Francisco, California.</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
