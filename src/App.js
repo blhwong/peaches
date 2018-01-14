58e4a04fdebdf6950e7cad2a40db7463214d578b
Brandon Wong <brandon@yerdle.com>
2018-01-13 18:45:05 -0800

    Implement sticky header

--- src/App.js
+++ src/App.js
@@ -2,20 +2,63 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headerHeight: undefined,
+      pageYOffset: undefined,
+    };
+
+    this.handleScroll = this.handleScroll.bind(this);
+    this.setHeight = this.setHeight.bind(this);
+    this.setBlockRef = this.setBlockRef.bind(this);
+  }
+
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  setHeight(headerHeight) {
+    this.setState({ headerHeight });
+  }
+
+  setBlockRef(input) {
+    this.block = input;
+  }
+
+  handleScroll() {
+    this.setState({ pageYOffset: window.pageYOffset });
+  }
+
   render() {
+    const offsetTop = (
+      (this.header && this.header.offsetTop) || (this.block && this.block.offsetTop)
+    );
+    const isSticky = this.state.pageYOffset >= offsetTop;
+    const headerClassName = isSticky ? 'sticky' : '';
     return (
       <div className="App">
         <div className="container">
           <div className="profile-container">
-          <img className="profile" alt="Brandon Wong" src="profile.jpg"/>
-          <Header />
-          <div className="sub-heading">I'm a software engineer, full-stack JavaScript developer, bird enthusiast, and a displaced Texan living in<br/>San Francisco, California.</div>
+            <img className="profile" alt="Brandon Wong" src="profile.jpg" />
+            <div className={headerClassName}>
+              <Header app={this} setHeight={this.setHeight} />
+            </div>
+            {isSticky && <div style={{ height: this.state.headerHeight }} ref={this.setBlockRef} />}
+            <div className="sub-heading">I'm a software engineer, full-stack JavaScript developer, bird enthusiast, and a displaced Texan living in<br />San Francisco, California.</div>
           </div>
         </div>
         <div className="container">
           Bio
+        </div>
+        <div className="container">
+          Experience
         </div>
         <div className="container">
           Projects
--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.css';
 import github from '../public/github.png';
 import linkedin from '../public/linkedin.png';
 import angellist from '../public/angellist.png';
 
-const Header = () => {
-  return (
-    <div className="header">
-      <h1 className="name">Brandon Wong</h1>
-      <span>
-        <a href="https://github.com/blhwong" className="links">
-          <img className="logo" src={github} alt="Github logo"/>
-        </a>
-        <a href="https://www.linkedin.com/in/blhwong/" className="links">
-          <img className="logo" src={linkedin} alt="LinkedIn logo"/>
-        </a>
-        <a href="https://angel.co/blhwong" className="links">
-          <img className="logo" src={angellist} alt="AngelList logo"/>
-        </a>
-      </span>
-    </div>
-  );
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.setHeaderRef = this.setHeaderRef.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.setHeight(this.header.clientHeight);
+  }
+
+  setHeaderRef(input) {
+    console.log('setting header ref', input.offsetTop);
+    this.props.app.header = input;
+    this.header = input;
+  }
+
+  render() {
+    return (
+      <div ref={this.setHeaderRef} className="header">
+        <h1 className="name" onClick={() => window.scrollTo(0, 0)}>Brandon Wong</h1>
+        <span>
+          <a href="https://github.com/blhwong" className="links" target="_blank" rel="noopener noreferrer">
+            <img className="logo" src={github} alt="GitHub logo" />
+          </a>
+          <a href="https://www.linkedin.com/in/blhwong/" className="links" target="_blank" rel="noopener noreferrer">
+            <img className="logo" src={linkedin} alt="LinkedIn logo" />
+          </a>
+          <a href="https://angel.co/blhwong" className="links" target="_blank" rel="noopener noreferrer">
+            <img className="logo" src={angellist} alt="AngelList logo" />
+          </a>
+        </span>
+      </div>
+    );
+
+  }
 }
 
 export default Header;
